2548bc6d97a6d17b9d274afe4cc2d3e58acbeaf1
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-05-14 09:01:04 -0600

    Routing and content on project page

--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,13 @@ const navigation = [
     { name: 'Contact', href: '/contact' },
 ]
 
+const bio = [
+  {
+    title: "Hi, My name is Iaroslav Lasiichuk I'm Frontend Developer",
+    content: "Hi, everyone!I'm a young aspiring junior web developer with a passion for technology and a strong desire to make a difference in the world. I'm constantly seeking new challenges and opportunities to learn and grow as a developer"
+  }
+]
+
 const posts = [
   {
     id: 1,
@@ -108,5 +115,56 @@ const posts = [
       },
     },
   
-  ]
-export default { navigation, posts };
+]
+const projects = [
+  {
+    id: 1,
+    name: 'Movie Blog',
+    description:'The application allows users to search for movies based on title. The application also allows for the user to register to our app to save movies watched, comment, and read critic reviews.',
+    href: 'https://intense-island-90811.herokuapp.com/',
+    imageSrc: 'https://blog.logrocket.com/wp-content/uploads/2020/12/javascript-logo.png',
+    imageAlt: "Javascript Logo",
+  },
+  {
+    id: 2,
+    name: 'The Tech Blog',
+    description:'This is a CMS-style blog site for tech developers who want to publish articles, blog posts, and their thoughts and opinions. This site provides a user-friendly interface for managing posts, comments, and user accounts',
+    href: 'https://intense-river-83466.herokuapp.com/',
+    imageSrc: 'https://nodejs.org/_next/static/media/node.8ddb71ae.png',
+    imageAlt: "Node js logo",
+   
+  },
+  {
+    id: 3,
+    name: 'Note Taker',
+    description:'TThis is a simple Note Taker application that allows small business owners to write and save notes to keep track of their tasks and organize their thoughts.',
+    href: 'https://desolate-journey-55866.herokuapp.com/',
+    imageSrc: '../assets/nodejs-express.png',
+    imageAlt: "Node js express logo",
+  },
+  {
+    id: 4,
+    name: 'Note Taker',
+    description:'TThis is a simple Note Taker application that allows small business owners to write and save notes to keep track of their tasks and organize their thoughts.',
+    href: 'https://desolate-journey-55866.herokuapp.com/',
+    imageSrc: '../assets/nodejs-express.png',
+    imageAlt: "Node js express logo",
+  },
+  {
+    id: 5,
+    name: 'Note Taker',
+    description:'TThis is a simple Note Taker application that allows small business owners to write and save notes to keep track of their tasks and organize their thoughts.',
+    href: 'https://desolate-journey-55866.herokuapp.com/',
+    imageSrc: '../assets/nodejs-express.png',
+    imageAlt: "Node js express logo",
+  },
+  {
+    id: 6,
+    name: 'Note Taker',
+    description:'TThis is a simple Note Taker application that allows small business owners to write and save notes to keep track of their tasks and organize their thoughts.',
+    href: 'https://desolate-journey-55866.herokuapp.com/',
+    imageSrc: '../assets/nodejs-express.png',
+    imageAlt: "Node js express logo",
+  },
+]
+export default { navigation, bio, posts, projects };
