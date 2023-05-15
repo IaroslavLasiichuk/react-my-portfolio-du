65059a63449c9a9b7fd66088728aef778c76f36d
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-05-14 20:27:48 -0600

    Some style on components

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -78,7 +78,7 @@ export default function Contact() {
                     id="email"
                     autoComplete="email"
                     required
-                    className="block w-full border-1 rounded-md px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full border-1 rounded-md px-3.5 py-2 text-gray-900 ring-1 ring-inset ring-gray-600"
                   />
                 </div>
               </div>
